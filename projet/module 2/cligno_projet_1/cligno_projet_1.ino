60f5041f2cf28fc045072e6fd2160c4678789eef
Damspau <44500326+Damspau@users.noreply.github.com>
2018-11-13 14:13:00 +0100

    add

--- projet/module 2/cligno_projet_1/cligno_projet_1.ino
+++ projet/module 2/cligno_projet_1/cligno_projet_1.ino
@@ -8,19 +8,5 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite (9, HIGH);
-  delay(100);
-  digitalWrite (9, LOW);
-  digitalWrite (10, HIGH);
-  delay(100);
-  digitalWrite (10, LOW);
-  digitalWrite (11, HIGH);
-  delay(100);
-  digitalWrite (11, LOW);
-  digitalWrite (12, HIGH);
-  delay(100);
-  digitalWrite (12, LOW);
-  digitalWrite (13, HIGH);
-  delay(100);
-  digitalWrite (13, LOW);
+
 }
--- projet/module 2/cligno_projet_1/param.c
+++ projet/module 2/cligno_projet_1/param.c
@@ -1,1 +1,18 @@
 
+void clignoChenille(void)
+{
+  digitalWrite (9, HIGH);
+  delay(100);
+  digitalWrite (9, LOW);
+  digitalWrite (10, HIGH);
+  delay(100);
+  digitalWrite (10, LOW);
+  digitalWrite (11, HIGH);
+  delay(100);
+  digitalWrite (11, LOW);
+  digitalWrite (12, HIGH);
+  delay(100);
+  digitalWrite (12, LOW);
+  digitalWrite (13, HIGH);
+  delay(100);
+  digitalWrite (13, LOW);
