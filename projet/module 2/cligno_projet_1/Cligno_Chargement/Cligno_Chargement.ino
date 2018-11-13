f5851ac81df88b41a3223bc659add0c272f2e5fc
Tangauche <44364688+Tangauche@users.noreply.github.com>
2018-11-13 14:07:30 +0100

    Yummy

---
+++ projet/module 2/cligno_projet_1/Cligno_Chargement/Cligno_Chargement.ino
@@ -0,0 +1,25 @@
+//Cligno6.ino
+int nLeds = 9 ;
+int ledPins[] = {5,6,7,8,9,10,11,12,13} ;
+int i ;
+void setup() 
+{                
+	for (i=0; i< nLeds; i++)
+	{
+		pinMode (ledPins[i], OUTPUT) ;
+	}
+}
+void loop() 
+{
+	for (i=0; i< nLeds ; i++)
+	{
+		digitalWrite (ledPins[i],LOW ); // allume
+		delay (1000) ;
+	}
+	for (i=nLeds-1; i >= 0 ; i--)
+	{
+		digitalWrite (ledPins[i],HIGH); // eteint
+		delay (1000) ;
+	}
+	delay (2000) ;
+}
