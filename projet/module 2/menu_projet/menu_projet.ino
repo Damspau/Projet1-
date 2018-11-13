230920b165a267730f1df6ddce2f7eb5bc2be1dc
Damspau <44500326+Damspau@users.noreply.github.com>
2018-11-13 21:23:13 +0100

    Update projet/module 2/menu_projet/menu_projet.ino

--- projet/module 2/menu_projet/menu_projet.ino
+++ projet/module 2/menu_projet/menu_projet.ino
@@ -10,11 +10,15 @@ void setup() {
 }
 
 void loop() {
+ Serial.begin(9600)
  int choix;
  choix=0;
- scanf("%d", &choix);
+Serial.print("Pour le mode chenille c'est 1 \n 2 pour le mode simultané \n 3 pour le 1 sur quelque chose \n 4 pour le mode progressif");
+Serial.read(choix);
  if (choix==1)
  {
    void clignoChenille(void);
  }
+/else if etc etc*/
+
 }
