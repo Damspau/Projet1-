7929470ebaeddc482d163617b28bd41d0a1bfd72
Damspau <44500326+Damspau@users.noreply.github.com>
2018-11-14 14:21:21 +0100

    Update projet/module 2/cligno_projet_1V2/param.h

--- projet/module 2/cligno_projet_1V2/param.h
+++ projet/module 2/cligno_projet_1V2/param.h
@@ -1,7 +1,10 @@
 #ifndef PARAM_H_INCLUDED
 #define PARAM_H_INCLUDED
+#include <time.h>
+#include <Arduino.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#include "param.c"
 
 void clignoChenille(void);
 void simultaneCligno (void);
@@ -9,7 +12,87 @@ void unSurAutreSimultane (int lequel);
 void unSurAutreChenille (int lequel);
 
 
+void clignoChenille(void)
+{
+ srand (time (NULL));
+ 
+ int i;
+ int delayrandom;
+ delayrandom = (rand()%(900-600+1))+600;
+  for (i=5;i<=13;i++)
+ {
+  digitalWrite (i, HIGH);
+  delay(delayrandom);
+  digitalWrite (i, LOW);
 
+}
+} 
+void simultaneCligno (void)
+ {
+  int i;
+ 
+    for (i=5;i<=13;i++)
+  {
+    digitalWrite (i,LOW ); // allume
+  
+  }
+  delay(700);
+  for (i=13;i>=5;i--)
+  {
+    digitalWrite (i,HIGH); // eteint
+ }
+ delay (300);
+ }
+
+ void progressif (void)
+ {
+
+  int i =0;
+  for (i=5;i<=13;i++)
+  {
+    digitalWrite (i,HIGH ); // allume
+    delay (1000) ;
+  }
+  for (i=13; i >= 5 ; i--)
+  {
+    digitalWrite (i,LOW); // eteint
+    delay (1000) ;
+  }
+ }
+
+ void unSurAutreSimultane (int lequel)
+{
+  int i;
+ 
+    for (i=5;i<=13;i=i+lequel)
+  {
+    digitalWrite (i,LOW ); // allume
+  
+  }
+  delay(700);
+  for (i>=13;i>=5;i=i-lequel)
+  {
+    digitalWrite (i,HIGH); // eteint
+ }
+ delay (300);
+ }
+
+
+void unSurAutreChenille (int lequel)
+{
+ srand (time (NULL));
+ 
+ int i;
+ int delayrandom;
+ delayrandom = (rand()%(900-600+1))+600;
+  for (i=5;i<=13;i=i+lequel)
+ {
+  digitalWrite (i, HIGH);
+  delay(delayrandom);
+  digitalWrite (i, LOW);
+
+}
+}  
 
 
 
