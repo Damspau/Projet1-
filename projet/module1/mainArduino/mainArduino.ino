f03bc3f38446b3f9fcde53125a60de9aa818c268
Damspau <44500326+Damspau@users.noreply.github.com>
2018-11-14 15:54:14 +0100

    Update projet/module1/mainArduino/mainArduino.ino

--- projet/module1/mainArduino/mainArduino.ino
+++ projet/module1/mainArduino/mainArduino.ino
@@ -11,14 +11,11 @@ Serial.begin(9600);
 
 void loop() {
 pulse=0;
-unsigned long time;
-millistart2= millistart;
-temps=millistart+1000;
-time= millis();
 
-temps=millis()+1000;
+
+
 millistart2= millis();
-while (millis()<millistart2+3000)
+while (millis()<millistart2+10000)
 {
  
  pulse=traitement()+pulse;
@@ -30,17 +27,16 @@ while (millis()<millistart2+3000)
 millisEnd=millis();      
 
 
-resultat=pulse*20;
+resultat=pulse*6;
 
 
-Serial.print("Voici le resultat\n");
+
 Serial.print(resultat);
-Serial.print(" battements minutes\n");
-Serial.print("Lance depuis\n");
+Serial.print(";");
 millisEnd = millis();
-millisEnd=millisEnd/1000.0;
+millisEnd=millisEnd;
 Serial.print(millisEnd);
-Serial.print(" s\n");
+
 
 
 
