72a171e96f03697cdea077cbf78c742b2a785ec7
The0a <44963952+The0a@users.noreply.github.com>
2018-11-16 01:17:43 +0100

    push

--- projet/module4/traitementdonnes/menu.c
+++ projet/module4/traitementdonnes/menu.c
@@ -1,4 +1,39 @@
+#include <stdio.h>
+#include "actions.h"
 int menu(void)
 {
-
+    int choix;
+    printf("quelle action ?");
+    printf("1/afficher les valeurs\n2/tri croissant du poul\n3/tri croissant du temps\n4/tri decroissant du poul\n5/tri decroissant du temps\n6/afficher la moyenne des pouls\n7/afficher le nombre de ligne\n8/afficher le minimum et le maximum\n");
+    scanf("%d", &choix);
+    switch(choix)
+    {
+    case 1:
+        afficher(*tableau);
+        break;
+    case 2:
+        trierCroissantPoul(*tableau);
+        break;
+    case 3:
+        trierCroissantTemps(*tableau);
+        break;
+    case 4:
+        trierDecroissantPoul(*tableau);
+        break;
+    case 5:
+        trierDecroissantTemps(*tableau);
+        break;
+    case 6:
+        afficherMoyennePouls(*tableau);
+        break;
+    case 7:
+        afficherLigne(*tableau);
+        break;
+    case 8:
+        afficherMaxOuMin(*tableau);
+        break;
+    default:
+        printf("cette action n'existe pas");
+        break;
+}
 }
