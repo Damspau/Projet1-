ff01fe71dd18790addc4387954521c90bb2c384c
Tangauche <44364688+Tangauche@users.noreply.github.com>
2018-11-16 12:03:23 +0100

    YES

--- projet/module2/MenuEnC/menu.c
+++ projet/module2/MenuEnC/menu.c
@@ -10,17 +10,17 @@ void affichageMenu (void)
     {
         printf("\n=== Menu ===\n\n");
         printf("1. Clignotement chenille\n");
-        printf("2. Clignotement simultans\n");
+        printf("2. Clignotement simultane\n");
         printf("3. Barre de chargement et dechargement LED\n");
         printf("4. Une LED sur autres LED en simultane\n");
         printf("5. Une LED sur autres LED en chenille\n");
-        printf("6.Fermeture du menu\n");
+        printf("0.Fermeture du menu\n");
         scanf("%d", &choixMenu);
         printf("\n");
 
-        if (choixMenu > 6)
+        if (choixMenu > 5)
         {
-            printf("Veuillez entrer une valeur comprise entre 0 et 6.\n");
+            printf("Veuillez entrer une valeur comprise entre 0 et 5.\n");
         }
         else
         {
@@ -56,15 +56,17 @@ void affichageMenu (void)
             }
             if (choixMenu == 6)
             {
-                if (fichierG == 0)
-                {
-                    printf("Le fichier param.h n'est pas present\n");
-                }
-
-                printf("Fermeture du programme");
+                
                 return ;
             }
         }
     }
-return;
+    if (fichierG == 0)
+    {
+         printf("Le fichier param.h n'est pas present\n");
+     }
+
+    printf("Fermeture du programme");
+                
+    return;
 }
